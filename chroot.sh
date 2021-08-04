7a5f2ab0f02a0ff1c8733f46b1e20dfc7190e8b0
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-08-04 19:39:37 +0200

    temporary 2

--- chroot.sh
+++ chroot.sh
@@ -3,6 +3,11 @@
 region=
 city=
 locale=
+hostname=
+microcode_updates=
+microcode=
+username=
+password=
 
 package_update() {
     pacman -Syu --noconfirm
@@ -30,6 +35,22 @@ network_configuration() {
     systemctl enable NetworkManager
 }
 
-user_setup() {
+boot_loader() {
+    pacman -S grub efibootmgr os-prober --noconfirm
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+    grub-mkconfig -o /boot/grub/grub.cfg
+}
 
+microcode_updates() {
+    if [[ $has_microcode_updates ]]; then
+        pacman -S "$microcode-ucode"
+    fi
+}
+
+user_setup() {
+    mkdir -p /home/konsta
+    useradd -m $username
+    echo "$username ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    echo -e "$password\n$password" | passwd $username
+    echo -e "$password\n$password" | passwd
 }
--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
-exec 3>&1
-array=(desktop laptop pi)
-result=$(dialog --title "Dot file installation"\
-    --radiolist "Select the branch of dot files for this computer" 15 60 3\
-    desktop "The branch used for desktop with nvidia card" on \
-    laptop "The branch used for a laptop with amd card" off \
-    pi "The branch used for a raspberry pi" off 2>&1 1>&3)
-clear
-echo $result
+# exec 3>&1
+# array=(desktop laptop pi)
+# result=$(dialog --title "Dot file installation"\
+#     --radiolist "Select the branch of dot files for this computer" 15 60 3\
+#     desktop "The branch used for desktop with nvidia card" on \
+#     laptop "The branch used for a laptop with amd card" off \
+#     pi "The branch used for a raspberry pi" off 2>&1 1>&3)
+# clear
+# echo $result
+
+
