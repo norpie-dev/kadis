50e569c0e6808cbb43e089130371c41558517e6c
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-04-13 19:14:14 +0200

    added virtualbox partitioning

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 selecting() {
     echo "Enter your drive devices without '/dev/' (eg. sda): "
-    read 
+    read
     TARGET=${REPLY} TARGET_DEVICE="/dev/$TARGET"
 }
 
-partition() {
+partition_physical() {
     # Partitioning of the disk
     # to create the partitions programatically (rather than manually)
     # we're going to simulate the manual input to fdisk
-    # The sed script strips off all the comments so that we can 
+    # The sed script strips off all the comments so that we can
     # document what we're doing in-line with the actual commands
     # Note that a blank line (commented as "defualt" will send a empty
     # line terminated with a newline to take the fdisk default.
-    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $TARGET_DEVICE 
+    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $TARGET_DEVICE
         d # delete partition
           # confirm deletion of partition 3
         d # delete partition
@@ -23,7 +23,7 @@ partition() {
         n # create new partition
           # confirm number 1
           # confirm first sector
-        +1G # confirm last sector 
+        +1G # confirm last sector
         y # confirm removal of signature
         n # create new partition
           # confirm number 2
@@ -34,17 +34,49 @@ partition() {
 EOF
 }
 
+partition_virtual() {
+    # Partitioning of the disk
+    # to create the partitions programatically (rather than manually)
+    # we're going to simulate the manual input to fdisk
+    # The sed script strips off all the comments so that we can
+    # document what we're doing in-line with the actual commands
+    # Note that a blank line (commented as "defualt" will send a empty
+    # line terminated with a newline to take the fdisk default.
+    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $TARGET_DEVICE
+        d # delete partition
+          # confirm deletion of partition 3
+        d # delete partition
+          # confirm deletion of partition 2
+        d # delete partition, automatically confirms partition 1
+        n # create new partition
+          # confirm primary 1
+          # confirm number 1
+          # confirm first sector
+        +1G # confirm last sector
+        y # confirm removal of signature
+        n # create new partition
+          # confirm primary
+          # confirm number 2
+          # confirm first sector
+          # confirm last sector
+        y # confirm removal of signature
+        w # save
+EOF
+}
+
 formatting() {
+    [[ "$TARGET_DEVICE" == *"nvme"* ]] && NVME="p"
     # Format Partitions
-    mkfs.vfat -F32 "$TARGET_DEVICE"1
-    mkfs.ext4 -F "$TARGET_DEVICE"2
+    mkfs.vfat -F32 "$TARGET_DEVICE"$NVME"1"
+    mkfs.ext4 -F "$TARGET_DEVICE"$NVME"2"
 }
 
 mounting() {
+    [[ "$TARGET_DEVICE" == *"nvme"* ]] && NVME="p"
     # Mount Partitions
-    mount "$TARGET_DEVICE"2 "/mnt"
+    mount "$TARGET_DEVICE"$NVME"2" "/mnt"
     mkdir -p "/mnt/boot"
-    mount "$TARGET_DEVICE"1 "/mnt/boot"
+    mount "$TARGET_DEVICE"$NVME"1" "/mnt/boot"
 }
 
 basing() {
