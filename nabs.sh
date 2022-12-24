b2ba47c64d60a4d6d3d0ddbe8bfc625f1b5b505e
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-12-24 15:53:27 +0100

    quick final and instruction

--- nabs.sh
+++ nabs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git clone https://github.com/norpie-dev/nabs && cd nabs && ./install.sh
+pacman-key --init && pacman -Syy git archlinux-keyring --noconfirm && git clone https://github.com/norpie-dev/nabs && cd nabs && ./install.sh
