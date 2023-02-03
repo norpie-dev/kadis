738a6cd8fe2c6d7d31069b61fd987408379266a1
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-02-03 22:52:30 +0100

    d

--- install.sh
+++ install.sh
@@ -33,16 +33,7 @@ selecting() {
     TARGET=${REPLY}
     TARGET_DEVICE="/dev/$TARGET"
 }
-
 partition() {
-    if [[ "$TARGET" == *"vd"* ]]; then
-        partition_virtual
-    else
-        partition_physical
-    fi
-}
-
-partition_physical() {
     # Partitioning of the disk
     # to create the partitions programatically (rather than manually)
     # we're going to simulate the manual input to fdisk
@@ -70,37 +61,6 @@ partition_physical() {
         w # save
         y # confirm removal of signature
         y # confirm removal of signature
-EOF
-}
-
-partition_virtual() {
-    # Partitioning of the disk
-    # to create the partitions programatically (rather than manually)
-    # we're going to simulate the manual input to fdisk
-    # The sed script strips off all the comments so that we can
-    # document what we're doing in-line with the actual commands
-    # Note that a blank line (commented as "defualt" will send a empty
-    # line terminated with a newline to take the fdisk default.
-    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $TARGET_DEVICE
-        g # gpt
-        d # delete partition
-          # confirm deletion of partition 3
-        d # delete partition
-          # confirm deletion of partition 2
-        d # delete partition, automatically confirms partition 1
-        n # create new partition
-          # confirm primary 1
-          # confirm number 1
-          # confirm first sector
-        +1G # confirm last sector
-        y # confirm removal of signature
-        n # create new partition
-          # confirm primary
-          # confirm number 2
-          # confirm first sector
-          # confirm last sector
-        y # confirm removal of signature
-        w # save
 EOF
 }
 
@@ -108,7 +68,7 @@ formatting() {
     [[ "$TARGET_DEVICE" == *"nvme"* ]] && NVME="p"
     # Format Partitions
     mkfs.vfat -F32 "$TARGET_DEVICE"$NVME"1"
-    cryptsetup luksFormat "$TARGET_DEVICE"
+    #cryptsetup luksFormat "$TARGET_DEVICE"1
     #mkfs.ext4 "$TARGET_DEVICE"$NVME"2"
     #mkfs.ext4 "$TARGET_DEVICE"$NVME"3"
 }
