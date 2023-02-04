6ea9d2a7a805fa59524cffba8bb7d05c6c5b9c2a
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-02-04 13:01:17 +0100

    d

--- install.sh
+++ install.sh
@@ -67,8 +67,7 @@ EOF
 formatting() {
     [[ "$TARGET_DEVICE" == *"nvme"* ]] && NVME="p"
     # Encrypt
-    echo $password
-    echo -e "YES\n$password\n$password" | cryptsetup luksFormat "$TARGET_DEVICE"2
+    echo -e "$password" | cryptsetup -q luksFormat "$TARGET_DEVICE"2
     echo -e "$password" | cryptsetup open "$TARGET_DEVICE"2 cryptlvm
     # Create volumes
     pvcreate /dev/mapper/cryptlvm
