2c7415aebdead089491675684ce868f3f5241c63
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-02-06 11:15:12 +0100

    refactor: use archinstall for user, password and hostname

--- install.sh
+++ install.sh
@@ -6,17 +6,8 @@ get_input() {
     returnvalue=${REPLY}
 }
 
-make_user() {
-    get_input "Enter your username"
-    username=$returnvalue
-    get_input "Enter your password"
-    password=$returnvalue
-    sed -i 's/PASSWORD/$password/g' user_credentials.json
-    sed -i 's/USERNAME/$username/g' user_credentials.json
-}
-
 prepare() {
     sed 's/#ParallelDownloads\ =\ 5/ParallelDownloads\ =\ 15/g' /etc/pacman.conf -i
 }
 
-make_user && prepare && sudo archinstall --config user_configuration.json --creds user_credentials.json
+prepare && sudo archinstall --config user_configuration.json --creds user_credentials.json
